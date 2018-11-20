d24523704c8d295858e1e8b3c1a71dd7eee6dac2
lunwingkit <35659238+lunwingkit@users.noreply.github.com>
2018-11-20 14:14:07 +0800

    added

--- unity_starbeat/Assets/Scripts/SongSelection/SetUpSongPlate.cs
+++ unity_starbeat/Assets/Scripts/SongSelection/SetUpSongPlate.cs
@@ -12,49 +12,57 @@ public class SetUpSongPlate : MonoBehaviour {
     Text bpm;
     public GameObject sample;
     public GameObject parent;
+    public static int x = 0;
 
 	// Use this for initialization
 	void Start () {
-        songPlate = GameObject.Find("SongPlate1");
-        songName = songPlate.transform.Find("TextSongName").GetComponent<Text>();
-        songWriter = songPlate.transform.Find("TextSongWriter").GetComponent<Text>();
-        achievementRate = songPlate.transform.Find("TextAchievementRate").GetComponent<Text>();
-        bpm = songPlate.transform.Find("TextBPM").GetComponent<Text>();
-
-        songName.text = "text1";
-        songWriter.text = "wr";
-        achievementRate.text = "100%";
-        bpm.text = "120";
 
         parent = GameObject.Find("SongPlateContainer");
-        CreateSongPlate();
-    }
-	
-	private void CreateSongPlate()
-    {
-        var plate = (GameObject)Instantiate(sample, parent.transform);
-        plate.transform.Find("TextSongName").GetComponent<Text>().text = "HIHAH";
-        plate.transform.Find("TextSongWriter").GetComponent<Text>().text = "HIHAH";
-        plate.transform.Find("TextAchievementRate").GetComponent<Text>().text = "HIHAH";
-        plate.transform.Find("TextBPM").GetComponent<Text>().text = "HIHAH";
-        plate.transform.localPosition = songPlate.transform.localPosition;
-        plate.transform.localRotation = songPlate.transform.localRotation;
-        plate.transform.localScale = songPlate.transform.localScale;
-    }
-
-
-    public void readJson(GameObject songPlate)
-    {
         string filePath = Application.dataPath + "/songPlateInfo.txt";
+        readJson(filePath);
+    }
+
+    public void readJson(string filePath)
+    {
         if (File.Exists(filePath))
         {
             string dataAsJson = File.ReadAllText(filePath);
-            Song loadData = JsonUtility.FromJson<Song>(dataAsJson);
-            songPlate.transform.Find("TextSongName").GetComponent<Text>().text = loadData.songName;
+            using (System.IO.StringReader reader = new System.IO.StringReader(dataAsJson))
+            {
+                List<Song> songList = new List<Song>();
+                while (reader.Peek() != -1)
+                {
 
+                    string songInfo = reader.ReadLine();
+                    string difficultyInfo = reader.ReadLine();
+                    Song newSong = JsonUtility.FromJson<Song>(songInfo);
+                    Difficulty songDifficulty = JsonUtility.FromJson<Difficulty>(difficultyInfo);
+                    newSong.difficulty = songDifficulty;
+                    songList.Add(newSong);
+                }
+                foreach (Song song in songList)
+                {
+                    createSongPlate(song);
+                }
+            }
         }
+        else
+            print("Cannot Find Json File");
     }
 
+    public void createSongPlate(Song song)
+    {
+        
+        var plate = (GameObject)Instantiate(sample, parent.transform);
+        plate.transform.Find("TextSongName").GetComponent<Text>().text = song.songName;
+        plate.transform.Find("TextSongWriter").GetComponent<Text>().text = song.songWriter;
+        plate.transform.Find("TextAchievementRate").GetComponent<Text>().text = "100%";
+        plate.transform.Find("TextBPM").GetComponent<Text>().text = song.bpm;
+        plate.transform.localPosition = new Vector2(sample.transform.localPosition.x + x*200, sample.transform.localPosition.y);
+        x++;
+        plate.transform.localRotation = sample.transform.localRotation;
+        plate.transform.localScale = sample.transform.localScale;
+    }
 }
 
 
@@ -64,14 +72,14 @@ public class Song
     public string songWriter;
     public string chartMaker;
     public string bpm;
-    public List<Difficulty> level;
+    public Difficulty difficulty;
 }
 
-public enum Difficulty
+public class Difficulty
 {
-    Easy,
-    Normal,
-    Hard
+    public string easy;
+    public string normal;
+    public string hard;
 }
 
 public class PlayRecord
@@ -81,3 +89,40 @@ public class PlayRecord
     public float achievment;
     public int combo;
 }
+
+/*
+            Song loadData = JsonUtility.FromJson<Song>(dataAsJson);
+            songPlate.transform.Find("TextSongName").GetComponent<Text>().text = loadData.songName;
+
+            Song test = new Song();
+            test.songName = "name";
+            test.songWriter = "";
+            test.chartMaker = "";
+            test.bpm = "";
+        test.difficulty = new Difficulty();
+        test.difficulty.easy = "5";
+        test.difficulty.normal = "7";
+        test.difficulty.hard = "10";
+        string json = JsonUtility.ToJson(test);
+        string j2 = JsonUtility.ToJson(test.difficulty);
+        print(json);
+        print(j2);
+        Song newSong = JsonUtility.FromJson<Song>(json);
+        print(newSong.songName);
+
+            */
+
+
+
+/*
+songPlate = GameObject.Find("SongPlate1");
+songName = songPlate.transform.Find("TextSongName").GetComponent<Text>();
+songWriter = songPlate.transform.Find("TextSongWriter").GetComponent<Text>();
+achievementRate = songPlate.transform.Find("TextAchievementRate").GetComponent<Text>();
+bpm = songPlate.transform.Find("TextBPM").GetComponent<Text>();
+
+songName.text = "text1";
+songWriter.text = "wr";
+achievementRate.text = "100%";
+bpm.text = "120";
+*/
