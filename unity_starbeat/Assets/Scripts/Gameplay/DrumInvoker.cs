3b082d7a0c220d50a8866b35c2ba64d2d093d77d
lunwingkit <35659238+lunwingkit@users.noreply.github.com>
2018-11-29 16:52:34 +0800

    Update DrumInvoker.cs

--- unity_starbeat/Assets/Scripts/Gameplay/DrumInvoker.cs
+++ unity_starbeat/Assets/Scripts/Gameplay/DrumInvoker.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class DrumInvoker : MonoBehaviour {
     public GameObject drum;
     public int selector;
+    public Material beforeHitMaterial;
+    public Material afterHitMaterial;
+    Renderer rend;
+	
 	// Use this for initialization
 	void Start () {
         drum = this.gameObject;
         selector = int.Parse(drum.name.Replace("Drum", ""));
+        rend = GentComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = beforeHitMaterial;
 	}
 	
 	// Update is called once per frame
@@ -22,14 +29,26 @@ public class DrumInvoker : MonoBehaviour {
         if(collision.gameObject.name == "Drumstick(Clone)" || collision.gameObject.name == "RigidRoundHand(Clone)")
         {
             print(selector.ToString() + " just hit");
+            rend.sharedMaterial = afterHitMaterial;
             AudioSoundEffect.instance.onPress(selector);
             GameObject.Find("NodeLine" + selector + "/TapPosition").GetComponent<CheckTiming>().invokeJudgment();
         }
     }
+	
+    void OnCollisionExit(Collision collision)
+    {
+        rend.sharedMaterial = beforeHitMaterial;
+    }
 
     void OnMouseDown()
     {
+        rend.sharedMaterial = afterHitMaterial;
         AudioSoundEffect.instance.onPress(selector);
         GameObject.Find("NodeLine" + selector + "/TapPosition").GetComponent<CheckTiming>().invokeJudgment();
+    }
+	
+    void OnMouseDown()
+    {
+        rend.sharedMaterial = beforeHitMaterial;
     }
 }
